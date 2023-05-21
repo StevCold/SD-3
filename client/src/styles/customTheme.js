2b1446a49ab42fc6d112dbc9c19a2a9af0cf8dfe
SteveCold <stefan_campan@yahoo.com>
2023-05-22 00:32:43 +0300

    assignment 3

---
+++ client/src/styles/customTheme.js
@@ -0,0 +1,51 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const customTheme = (darkMode) =>
+  createMuiTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: darkMode ? '#ffffff' : '#000000',
+      },
+      secondary: {
+        main: darkMode ? '#ff9999' : '#ff3333',
+      },
+    },
+    overrides: {
+      MuiMenuItem: {
+        root: {
+          '&$selected': {
+            borderRight: '4px solid #e65c00',
+            fontWeight: '700',
+          },
+        },
+      },
+      MuiPopover: {
+        paper: {
+          borderRadius: 2,
+        },
+      },
+      MuiButton: {
+        root: {
+          borderRadius: 4,
+          textTransform: 'none',
+        },
+      },
+      MuiChip: {
+        root: {
+          borderRadius: 3,
+          padding: '0px',
+        },
+        outlined: {
+          backgroundColor: darkMode ? '#ff9999' : '#ff3333',
+        },
+      },
+    },
+    props: {
+      MuiButton: {
+        disableElevation: true,
+      },
+    },
+  });
+
+export default customTheme;
